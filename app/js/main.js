f155c2bb8f9b96148e549c9a68e527e309ad8247
b2b <mail@b2b.ru>
2021-08-17 13:11:49 +0300

    доделываю последний экран, остается анимация

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
 
+document.addEventListener("DOMContentLoaded", function(event) {
+	const swiper = new Swiper(".swiper-container", {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		speed: 800,
+		pagination: {
+			el: ".discount__pagination",
+		},
+		// autoplay: {
+		// 	delay: 2500,
+		// 	disableOnInteraction: false,
+		// },
+	});
+});
